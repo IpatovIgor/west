67b2a91d7571a08dbb7acd9722be78c4708d0702
Igor Ipatov <ipatovi142@gmail.com>
2026-03-17 19:26:05 +0500

    done5

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import Card from './Card.js';
 import Game from './Game.js';
 import TaskQueue from './TaskQueue.js';
 import SpeedRate from './SpeedRate.js';
+import Creature from './Card.js';
 
 // Отвечает является ли карта уткой.
 function isDuck(card) {
@@ -47,15 +48,40 @@ class Dog extends Card {
     }
 }
 
+class Gatling extends Creature {
+    constructor(name = "Гатлинг", level = 6, image = 'bandit.png') {
+        super(name, level, image);
+    }
+
+    attack(gameContext, continuation) {
+        const taskQueue = new TaskQueue();
+        const oppositePlayer = gameContext.oppositePlayer;
+
+        taskQueue.push(onDone => this.view.showAttack(onDone));
+
+        const cards = oppositePlayer.table.filter(card => card !== null && card !== undefined);
+
+        cards.forEach(card => {
+            taskQueue.push(onDone => {
+                this.dealDamageToCreature(2, card, gameContext, onDone);
+            });
+        });
+
+        taskQueue.continueWith(continuation);
+    }
+}
+
 // Замена карт в колодах
 const seriffStartDeck = [
-    new Duck('Мирная утка', 2),
-    new Duck('Мирная утка', 2),
-    new Duck('Мирная утка', 2),
+    new Gatling(),
+    new Duck(),
+    new Duck(),
+    new Duck(),
 ];
 
 const banditStartDeck = [
-    new Dog('Пес-бандит', 3),
+    new Dog(),
+    new Dog(),
 ];
 
 // Создание игры.
@@ -68,3 +94,5 @@ SpeedRate.set(1);
 game.play(false, (winner) => {
     alert('Победил ' + winner.name);
 });
+
+
